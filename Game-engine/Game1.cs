be6ae915887116a58d7ccfc318c2adde2015adc1
matheusleobelo <bipoladogamer@gmail.com>
2024-04-08 11:49:11 -0300

    projeto-base

---
+++ Game-engine/Game1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game_engine;
+
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+
+
+    private Texture2D _background;
+    private Texture2D _ship;
+    private Vector2 _shipPosition;
+    private float _shipSpeed;
+
+
+
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+
+        _shipPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+
+        _shipSpeed = 5.0f;
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        _background = Content.Load<Texture2D>("stars");
+        _ship = Content.Load<Texture2D>("ship");
+
+
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+
+            KeyboardState keyboardState = Keyboard.GetState();
+        if (keyboardState.IsKeyDown(Keys.A))
+        {
+            _shipPosition.X -= _shipSpeed;
+        }
+        if (keyboardState.IsKeyDown(Keys.D))
+        {
+            _shipPosition.X += _shipSpeed;
+        }
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+        _spriteBatch.Draw(_ship, _shipPosition, Color.White);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
