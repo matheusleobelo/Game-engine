1ec5cdd7b20729878fd7cec6ae3f8d915c91d0f3
Your Nametheualves <you@example.commatheusalves2906@gmail.com>
2024-04-13 11:34:10 -0300

    movimentação na tela de fundo

--- Game-engine/Game1.cs
+++ Game-engine/Game1.cs
@@ -9,13 +9,15 @@ namespace Game_engine
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private Texture2D _background;
+        private Vector2 _backgroundPosition = Vector2.Zero; // Posição do fundo
         private Ship _ship;
         private Spider _spider;
 
-         // Adicione uma textura para o projétil
+        // Adicione uma textura para o projétil
         private Texture2D _projectileTexture;
 
-
+        // Velocidade de movimento do fundo
+        private const float BACKGROUND_SPEED = 100.0f;
 
         public Game1()
         {
@@ -35,11 +37,12 @@ namespace Game_engine
             Globals.SCREEN_WIDTH = _graphics.PreferredBackBufferWidth;
             Globals.SCREEN_HEIGHT = _graphics.PreferredBackBufferHeight;
 
-
             _ship = new Ship(Content.Load<Texture2D>("ship"), new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight - 130), 5.0f, _projectileTexture);
             _spider = new Spider(Content.Load<Texture2D>("spider"), new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight - 880), 3.0f);
             _spider.Initialize();
 
+            // Define a posição inicial do fundo 
+            _backgroundPosition = new Vector2(0, -(_background.Height - _graphics.PreferredBackBufferHeight));
         }
 
         protected override void LoadContent()
@@ -58,11 +61,17 @@ namespace Game_engine
 
             base.Update(gameTime);
 
+            // move a imagm de fundo para baixo
+            _backgroundPosition.Y += BACKGROUND_SPEED * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //  reinicia a posição da imagem
+            if (_backgroundPosition.Y >= 0)
+                _backgroundPosition.Y = -(_background.Height - _graphics.PreferredBackBufferHeight);
+
             _ship.Update(gameTime);
             _spider.Update(gameTime);
 
             _ship.HasCollided(_spider);
-
         }
 
         protected override void Draw(GameTime gameTime)
@@ -70,7 +79,7 @@ namespace Game_engine
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+            _spriteBatch.Draw(_background, _backgroundPosition, Color.White); // Desenha o fundo na posição atual
             _ship.Draw(_spriteBatch);
             _spider.Draw(_spriteBatch);
             _spriteBatch.End();
