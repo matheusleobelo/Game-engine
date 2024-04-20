823cea34e397456ec1159c6cd5213b440d9cbb63
Your Nametheualves <you@example.commatheusalves2906@gmail.com>
2024-04-20 01:59:30 -0300

    menu estilizado

--- Game-engine/Screens/MenuScreen.cs
+++ Game-engine/Screens/MenuScreen.cs
@@ -5,19 +5,21 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Game_engine;
 public class MenuScreen : IScreen
 {
+    private Texture2D _backgroundTexture;
     private Button _playButton;
     private Button _exitButton;
 
     public void LoadContent(ContentManager content)
     {
+        _backgroundTexture = content.Load<Texture2D>("menu-background");
         _playButton = new Button(content.Load<Texture2D>("play_button"), Play);
         _exitButton = new Button(content.Load<Texture2D>("exit_button"), Exit);
     }
 
     public void Initialize()
     {
-        _playButton.Position = new Point((Globals.SCREEN_WIDTH - _playButton.Bounds.Width) / 2, 200);
-        _exitButton.Position = new Point((Globals.SCREEN_WIDTH - _exitButton.Bounds.Width) / 2, 250);
+        _playButton.Position = new Point((Globals.SCREEN_WIDTH - _playButton.Bounds.Width) / 2, 450);
+        _exitButton.Position = new Point((Globals.SCREEN_WIDTH - _exitButton.Bounds.Width) / 2, 500);
     }
 
     public void Update(float deltaTime)
@@ -28,6 +30,7 @@ public class MenuScreen : IScreen
 
     public void Draw(SpriteBatch spriteBatch)
     {
+        spriteBatch.Draw(_backgroundTexture, Vector2.Zero, Color.White);
         _playButton.Draw(spriteBatch);
         _exitButton.Draw(spriteBatch);
     }
--- Game-engine/obj/Debug/net6.0/Game-engine.AssemblyInfo.cs
+++ Game-engine/obj/Debug/net6.0/Game-engine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game-engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+990ef3b83293125c92e3f05c3678420ce47d98df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2989e0399ec45f926998f60dce4fe964ba3ba36d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game-engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game-engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
